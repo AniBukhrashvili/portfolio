6fec8c7d73af885f539cf3d5ed69c92efbb343cd
AniBukhrashvili <ana.bukhrashvili.1@btu.edu.ge>
2023-02-18 22:27:05 +0400

    add header and home page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+import logo from "../../assets/logo.webp";
+
+const Header = () => {
+  const [menu, showMenu] = useState(false);
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <img src={logo} className="nav__ab" />
+
+        <div className={menu ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#projects" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Projects
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!menu)}
+          ></i>
+        </div>
+
+        <nav className="nav__toggle" onClick={() => showMenu(!menu)}>
+          <i className="uil uil-apps"></i>
+        </nav>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,23 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/ani-bukhrashvili-a5313622b/"
+        className="home__social-icon"
+        target={"_blank"}
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/AniBukhrashvili"
+        className="home__social-icon"
+        target={"_blank"}
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
